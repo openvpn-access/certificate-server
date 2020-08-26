5b506bd59be9d0d3f30a75be330aff6dc007d4b6
Nathan Seymour <nathan@seymour.global>
2020-08-26 12:53:13 +0200

    Add automatic restart to allow openvpn time to generate pki

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -11,9 +11,21 @@ if [[ ! -f $CONFIG_FILE ]]; then
 fi
 
 # Start certificate-server
-# shellcheck disable=SC2046
-/opt/app/certificate_server --port=$(jq -r .port $CONFIG_FILE) \
-                            --cpu_cores=$(jq -r .cpu_cores $CONFIG_FILE) \
-                            --ca_cert=$(jq -r .pki.ca_cert $CONFIG_FILE) \
-                            --ca_key=$(jq -r .pki.ca_key $CONFIG_FILE) \
-                            --ta=$(jq -r .pki.ta $CONFIG_FILE)
+while [ true ]
+do
+    echo "Starting certificate-server..."
+    # shellcheck disable=SC2046
+    /opt/app/certificate_server --port=$(jq -r .port $CONFIG_FILE) \
+                                --cpu_cores=$(jq -r .cpu_cores $CONFIG_FILE) \
+                                --ca_cert=$(jq -r .pki.ca_cert $CONFIG_FILE) \
+                                --ca_key=$(jq -r .pki.ca_key $CONFIG_FILE) \
+                                --ta=$(jq -r .pki.ta $CONFIG_FILE)
+
+    if [[ ! $? -eq 0 ]]; then
+        echo "certificate-server crashed... Restarting in 5 seconds..."
+        sleep 3
+    else
+        exit 0
+    fi
+done
+
